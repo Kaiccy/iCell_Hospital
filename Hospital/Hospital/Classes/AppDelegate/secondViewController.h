f0ad534bb932e4cd954e5bd87c6bd1ab73886895
Tianqi Zhang <1120404010@qq.com>
2015-11-09 11:53:17 +0800

    11.9 11:53 张天琦 测试

---
+++ Hospital/Hospital/Classes/AppDelegate/secondViewController.h
@@ -0,0 +1,13 @@
+//
+//  secondViewController.h
+//  Hospital
+//
+//  Created by 张天琦 on 15/11/9.
+//  Copyright © 2015年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface secondViewController : UIViewController
+
+@end
